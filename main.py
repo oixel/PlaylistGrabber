4b80b4fd20435582f914b17c2bbf20a776eb3e86
oickle <oycell@aol.com>
2024-06-23 17:05:07 -0400

    Add comments and remove unneeded comments

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from renamer import *
 #TEST_PLAYLIST_URL = "https://www.youtube.com/watch?v=6S20mJvr4vs&list=PLRwfuN7siOr7p044Iw_7jfEhGPgfVdST-"  # IGOR
 TEST_PLAYLIST_URL = "https://www.youtube.com/playlist?list=PLfiMjLyNWxebu5tK9xUbPKcVeVpOwc8QI"  # CMIYGL
 
-# Cha
+# Change these constants to change what metadata is set
 SET_TRACK_NUMBERS = True
 SET_COVER_SOURCE = True
 SET_COVER_ART = False
@@ -23,9 +23,6 @@ if __name__ == "__main__":
     track_num = 1
 
     playlist = Playlist(TEST_PLAYLIST_URL)
-    #
-    # Weird bug where Put Me Through it is not getting album cover art and the COVER_SOURCE does not show, but does exist
-    # Unsure how to fix, could just ignore and move on to GUI Stuff I guess. Maybe delete eyed3 stuff entirely (?)
     #
     # Need to fix: NFR explicit not containing any data ?? Find another album that this happens on
     #
@@ -68,30 +65,3 @@ if __name__ == "__main__":
                 eyed3_mp3.tag.save(version=eyed3.id3.ID3_V2_4)
 
         print(file_name, "downloaded and written!\n")
-
-# Possible Reference for embedding album art:
-# https://stackoverflow.com/questions/50437358/c-sharp-taglib-set-album-cover-for-mp3
-
-#
-#
-# Potential alternative:
-#
-# Save url to image in ID3 data,
-# then follow this to convert url to surface on pygame player
-# https://stackoverflow.com/questions/57023015/convert-image-from-request-to-pygame-surface
-#
-# Yeah, just do that. Instead of downloading each album cover, just store the source of the cover onto it
-#
-#
-#
-# Here is how to read image data from song:
-# from PIL import Image
-# stream = BytesIO(mp3.tags["COVER_DATA"])
-#
-# image = Image.open(stream).convert("RGBA")
-# stream.close()
-# image.show()
-#
-#
-# Alternative to this is just using requests again and using mp3.tags["COVER_SOURCE"] to pull image data like i did in embedding
-# Honestly I think just reading that link and using requests would be so much cleaner than storing a giant block of bytes in text
