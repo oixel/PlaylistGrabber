ebaaabc42ebfbd0b98d3ac41685979b08eb3de5b
oickle <oycell@aol.com>
2024-06-27 19:13:40 -0400

    Implement basic title  metadata writing with mutagen

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import data_grabber as DG
 from downloader import *
 from pytube import *
-import taglib
-from io import BytesIO
-import eyed3
-from eyed3.id3.frames import ImageFrame
+from mutagen.id3 import ID3, TIT2
 from renamer import *
 import os
 
@@ -58,28 +55,9 @@ if __name__ == "__main__":
         
         download_song(song_url, path, file_name)
 
-        # Writes basic info into MP3's ID3 metadata
-        with taglib.File(f"{path}{file_name}.mp3", save_on_exit=True) as mp3:
-            mp3.tags["TITLE"] = [data["title"]]
-            mp3.tags["ALBUM"] = [data["album"]]
-            mp3.tags["ARTIST"] = [data["artist"]]
+        audio = ID3()
+        audio.add(TIT2(encoding=3, text=f"{data["title"]}"))  # THIS IS WORKING??? WRITING TITLE
+        audio.save(f"{path}{file_name}.mp3")
+        #audio.add_tags()
 
-            # Optional tags   
-            if SET_TRACK_NUMBERS:
-                mp3.tags['TRACKNUMBER'] = [f"{track_num}"]
-                track_num += 1
-            if SET_COVER_SOURCE:
-                mp3.tags["COVER_SOURCE"] = [data["cover_src"]]
-
-        if SET_COVER_ART:
-            # Reads and store byte data for album cover image
-            cont = requests.get(data["cover_src"]).content
-            image_bytes = BytesIO(cont).read()
-
-            # Writes image data to mp3 file and saves it if file can be read by eyed3
-            eyed3_mp3 = eyed3.load(f"{path}{file_name}.mp3")
-            if eyed3_mp3 != None:
-                eyed3_mp3.tag.images.set(ImageFrame.FRONT_COVER, image_bytes, "image/jpeg")
-                eyed3_mp3.tag.save(version=eyed3.id3.ID3_V2_4)
-
-        print(file_name, "downloaded and written!\n")
+        print(file_name, "downloaded!\n")
