7254fd8d254fedc54c6e545d69eece40acfe7270
oickle <oycell@aol.com>
2024-06-28 00:09:19 -0400

    Implement downloading multiple playlists in one run

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import data_grabber as DG
 from downloader import *
+from renamer import *
 from pytube import *
 from mutagen.easyid3 import EasyID3
-from renamer import *
-import os
 
 #TEST_PLAYLIST_URL = "https://www.youtube.com/playlist?list=OLAK5uy_kq_gJdWJ9LUwgYzXMWeocvSyee4OqsvOQ"  # NFR
 
@@ -14,58 +13,84 @@ import os
 # Change these constants to change what metadata is set
 SET_TRACK_NUMBERS = True
 SET_COVER_SOURCE = False
-SET_COVER_ART = False
+SET_COVER_ART = True
 
 DOWNLOAD_COVERS = False
 
 if __name__ == "__main__":
-    # Gets inputs for the playlist to download and what folder to download to
-    playlist_url = input("What is your desired YouTube playlist URL? ")
-    output_path = input("What is your output folder's name? ")
-    path = f"content/songs/"
+    # Loops until a proper integer is given
+    while True:
+        try:
+            playlist_count = int(input("How many playlists are being downloaded? "))
+            break
+        except ValueError:
+            print("Please input an integer.")
 
-    # Creates a list of all folders in desired path
-    folders = output_path.split('/')
-    
-    # Runs through all the folders in the desired path
-    for folder in folders:
-        # Checks if path exists in content folder, if not, create new folder
-        if not os.path.exists(f"{path}/"):
-            os.mkdir(path)
-        # Adds folder to complete output path
-        path += f"{folder}/"
+    # Stores tuples of format (YouTube link, output path)
+    playlist_paths = []
 
-    # Only used if SET_TRACK_NUMBERS is set to True
-    track_num = 1
-
-    playlist = Playlist(playlist_url)
-    #
-    # Need to fix: NFR explicit not containing any data ?? Find another album that this happens on
-    #
-    for song_url in playlist.video_urls:
-        dg = DG.DataGrabber(song_url)
-        data = dg.get_data()
-        file_name = rename(data["title"])
+    # Fills playlist_paths with URLs and output paths
+    for i in range(playlist_count):
+        # Gets inputs for the playlist to download and what folder to download to
+        playlist_url = input(f"What is your desired YouTube playlist URL for playlist {i + 1}? ")
+        output_path = input(f"What is your output path for playlist {i + 1}? ")
         
-        print(f"Downloading...{file_name}")
+        # Adds content/songs/ to the beginning of whatever desired path was inputted
+        path = f"content/songs/{output_path}"
 
-        # Downloads covers if desired
-        if DOWNLOAD_COVERS:
-            download_cover(data["cover_src"], data["album"])
+        # If path does not end with slash, add one to it
+        if path[-1] != '/':
+            path += '/'
         
-        download_song(song_url, path, file_name)
+        # Appends playlist url to recently created path
+        playlist_paths.append((playlist_url, path))
 
-        audio = EasyID3()
-        audio["title"] = f"{data["title"]}" # THIS IS WORKING??? WRITING TITLE
-        audio["artist"] = f"{data["artist"]}"
-        audio["albumartist"] = f"{data["artist"]}"
-        audio["album"] = f"{data["album"]}"
+    # Loops through all the URL / output path pairs in list
+    for pair in playlist_paths:
+        # Only used if SET_TRACK_NUMBERS is set to True
+        track_num = 1
 
-        if SET_TRACK_NUMBERS:
-            audio["tracknumber"] = f"{track_num}"
-            track_num += 1
+        # Pulls values from the tuple into seperated variables
+        playlist_url, path = pair
 
-        audio.save(f"{path}{file_name}.mp3")
-        #audio.add_tags()
+        # Creates playlist object in pytube from URL
+        playlist = Playlist(playlist_url)
 
-        print(file_name, "downloaded!\n")
+        # Loops through URLs in playlist--downloading and writing metadata for each
+        for song_url in playlist.video_urls:
+            # Grabs data from YouTube video and stores it in DataGrabber object
+            dg = DG.DataGrabber(song_url)
+            data = dg.get_data()
+
+            # Creates a appropriate file name with illegal characters and spaces removed
+            file_name = rename(data["title"])
+            
+            print(f"Downloading {file_name} by {data["artist"]}...")
+
+            # Downloads covers if desired
+            if DOWNLOAD_COVERS:
+                download_cover(data["cover_src"], data["album"])
+            
+            # Downloads song using pytube
+            download_song(song_url, path, file_name)
+
+            # Creates an EasyID3 object and edits their metadata using mutagen
+            audio = EasyID3()
+            audio["title"] = f"{data["title"]}"
+            audio["artist"] = f"{data["artist"]}"
+            audio["albumartist"] = f"{data["artist"]}"
+            audio["album"] = f"{data["album"]}"
+
+            # If track numbers are desired, write track number into metadata and increment track number
+            if SET_TRACK_NUMBERS:
+                audio["tracknumber"] = f"{track_num}"
+                track_num += 1
+
+            # Saves metadata into proper MP3 file
+            audio.save(f"{path}{file_name}.mp3")
+
+            print(file_name, "downloaded and written!\n")
+
+        print(f"All songs in playlist downloaded!\n------------\n")
+
+    print("ALL playlists done downloading! Enjoy :)")
