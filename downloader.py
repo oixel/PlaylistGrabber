fc55773b66e8f544588545bdf31eefd0619f5f1d
oickle <oycell@aol.com>
2024-06-29 13:45:39 -0400

    Fix fake MP3 issue!

--- downloader.py
+++ downloader.py
@@ -1,5 +1,6 @@
 import requests
 from pytubefix import YouTube
+from pydub import AudioSegment
 
 # Takes in image source URL and downloads it at parameterized path
 def download_cover(url, song_name) -> None:
@@ -9,13 +10,21 @@ def download_cover(url, song_name) -> None:
     path =  f'content/album_covers/{song_name}.jpg'
     open(path, "wb").write(image_data)
 
+# Utilizes pydubs' AudioSegment class to convert fake MP3 downloaded from YouTube audio to recognizable MP3 file
+def convert_to_proper_mp3(path) -> None:
+    AudioSegment.from_file(path).export(path, format="mp3")
+
 # Downloads song at given url
 def download_song(url, path, song_name) -> None:
     # Downloads an MP3 of the YouTube video audio from url to content/songs folder
     youtube = YouTube(url, use_oauth=True, allow_oauth_cache=True)
 
+    # Downloads audio of video as fake MP3 file
     stream = youtube.streams.get_audio_only()
     stream.download(f"{path}", f"{song_name}", mp3=True)
+
+    # Converts fake MP3 from video audio to proper MP3 file
+    convert_to_proper_mp3(f"C:/Users/oycel/Code/Projects/PlaylistGrabber/{path}{song_name}.mp3")
 
 # Returns YouTube object to access basic video information for filler metadata
 def get_song(url) -> None:
